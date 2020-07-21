2af971756f724899013fe3c82ad5aa308688e079
ThechoiceMaster <68073926+ThechoiceMaster@users.noreply.github.com>
2020-07-22 01:37:48 +0700

    register

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
 const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = "mongodb://localhost:27017/theChoice";
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("mongodb is running..."))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, console.log("server is running..."));
+app.use("/api/auth", require("./auth"));
+app.use(express.urlencoded({ extended: true }));
