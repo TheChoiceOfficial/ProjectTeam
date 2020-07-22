3d91a5390c4b1582bca3a4ddccbcf9612ea1fd18
ThechoiceMaster <68073926+ThechoiceMaster@users.noreply.github.com>
2020-07-22 12:00:42 +0700

    เรียกใช้ ID

--- auth.js
+++ auth.js
@@ -5,35 +5,33 @@ const bcrypt = require("bcryptjs");
 
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  User.findOne({ email: email }).then((user) => {
+  const user = await User.findOne({ email: email });
+  if (user) {
+    res.json({ message: "มี Email นี้ในระบบแล้ว" });
+  } else {
+    const user = await User.findOne({ name: name });
     if (user) {
-      res.json({ message: "มี Email นี้ในระบบแล้ว" });
+      res.json({ message: "มี Username นี้ในระบบแล้ว" });
     } else {
-      User.findOne({ name: name }).then((user) => {
-        if (user) {
-          res.json({ message: "มี Username นี้ในระบบแล้ว" });
-        } else {
-          const newUser = new User({
-            name,
-            email,
-            password,
-          });
-          bcrypt.genSalt(10, (err, salt) =>
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-              if (err) throw err;
-              newUser.password = hash;
-              newUser
-                .save()
-                .then((user) => {
-                  res.send(["สมัคสมาชิกสำเร็จแล้ว", user]);
-                })
-                .catch((err) => console.log(err));
-            })
-          );
-        }
+      const newUser = new User({
+        name,
+        email,
+        password,
       });
+      bcrypt.genSalt(10, (err, salt) =>
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => {
+              res.send(user._id);
+            })
+            .catch((err) => console.log(err));
+        })
+      );
     }
-  });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const db = "mongodb://localhost:27017/theChoice";
 
+app.use(express.urlencoded({ extended: true }));
 mongoose
   .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("mongodb is running..."))
@@ -10,4 +11,3 @@ mongoose
 
 app.listen(process.env.PORT || 5000, console.log("server is running..."));
 app.use("/api/auth", require("./auth"));
-app.use(express.urlencoded({ extended: true }));
